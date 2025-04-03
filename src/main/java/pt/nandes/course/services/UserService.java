5dde958b3d6c96c34fe990e9959484a1286fd14f
Bruno Fernandes <1191264@isep.ipp.pt>
2025-04-04 00:19:36 +0100

    Service layer, component registration

---
+++ src/main/java/pt/nandes/course/services/UserService.java
@@ -0,0 +1,25 @@
+package pt.nandes.course.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pt.nandes.course.entities.User;
+import pt.nandes.course.repositories.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> user = repository.findById(id);
+        return user.get();
+    }
+}
