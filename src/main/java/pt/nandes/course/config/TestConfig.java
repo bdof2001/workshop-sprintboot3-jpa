950b8f8281458acc7d1b9693cd47f6eb1b44a913
Bruno Fernandes <1191264@isep.ipp.pt>
2025-04-04 00:59:55 +0100

    OrderStatus enum

--- src/main/java/pt/nandes/course/config/TestConfig.java
+++ src/main/java/pt/nandes/course/config/TestConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import pt.nandes.course.entities.User;
 import pt.nandes.course.entities.Order;
+import pt.nandes.course.entities.enums.OrderStatus;
 import pt.nandes.course.repositories.OrderRepository;
 import pt.nandes.course.repositories.UserRepository;
 
@@ -31,9 +32,9 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
