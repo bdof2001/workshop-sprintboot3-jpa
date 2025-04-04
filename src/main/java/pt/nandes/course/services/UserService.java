11e0f6beb95317b4e5c0792b70d89a89914e92b5
Bruno Fernandes <1191264@isep.ipp.pt>
2025-04-04 12:54:13 +0100

    Exception handling - findById

--- src/main/java/pt/nandes/course/services/UserService.java
+++ src/main/java/pt/nandes/course/services/UserService.java
@@ -1,9 +1,12 @@
 package pt.nandes.course.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import pt.nandes.course.entities.User;
 import pt.nandes.course.repositories.UserRepository;
+import pt.nandes.course.services.exceptions.ResourceNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,7 +23,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = repository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
